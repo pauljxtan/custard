6527839a01d9c4e81ea92874f461865f4738390f
Paul Tan <pjxtan@gmail.com>
2017-02-19 16:28:13 -0500

    Insert test rows

--- sql/tasks.sql
+++ sql/tasks.sql
@@ -5,3 +5,7 @@ CREATE TABLE tasks (
   description TEXT(65535),
   dueDate DATE
 );
+
+INSERT INTO tasks VALUES ('Do thing', 'Do a thing', '2018-01-01');
+INSERT INTO tasks VALUES ('Do thing', 'Do another thing', '2019-01-01');
+INSERT INTO tasks VALUES ('Do thing', 'Do one more thing', '2020-01-01');
