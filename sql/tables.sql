cc10b61e37aa731fcf153de985e94143b30761b4
Paul Tan <pjxtan@gmail.com>
2017-03-19 12:14:43 -0400

    Added completed and archived flags to table; added jquery-ui

--- sql/tables.sql
+++ sql/tables.sql
@@ -5,9 +5,11 @@ CREATE TABLE tasks (
   title VARCHAR(255) NOT NULL,
   description TEXT(65535) NULL,
   dueDate DATE NULL,
+  completed BOOL NOT NULL,
+  archived BOOL NOT NULL,
   PRIMARY KEY (id)
 );
 
-INSERT INTO tasks (title, description, dueDate) VALUES ('Do thing', 'Do a thing', '2018-01-01');
-INSERT INTO tasks (title, description, dueDate) VALUES ('Do thing', 'Do another thing', '2019-01-01');
-INSERT INTO tasks (title, description, dueDate) VALUES ('Do thing', 'Do one more thing', '2020-01-01');
+INSERT INTO tasks (title, description, dueDate, completed, archived) VALUES ('Do thing', 'Do a thing', '2018-01-01', 0, 0);
+INSERT INTO tasks (title, description, dueDate, completed, archived) VALUES ('Do thing', 'Do another thing', '2019-01-01', 0, 0);
+INSERT INTO tasks (title, description, dueDate, completed, archived) VALUES ('Do thing', 'Do one more thing', '2020-01-01', 0, 0);
